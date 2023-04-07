53497e986d9e8603a4c5a155393da6958231247c
Yari <narducciyarigmail.com>
2023-04-07 17:56:27 +0200

    prova metodo iva

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -1,19 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace csharp_oop_shop
 {
-    internal class Prodotto
+    public class Prodotto
     {
-        public int codice;
+        private int codice;
         public string nome;
         public string descrizione;
         public decimal prezzo;
         public int iva;
+        public decimal prezzoIva;
+        Random rnd = new Random();
+        public int getCodice()
+        {
+          
 
+            return codice;
+        }
+
+        public int getPrezzoIva()
+        {
+            
+
+            return prezzoIva;
+        }
+
+        public Prodotto(string nome, string descrizione, decimal prezzo , int iva)
+        {
+            
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo= prezzo;
+            this.iva = iva;
+            Random rnd = new Random();
+            codice = rnd.Next(1, 11);
+            prezzoIva= prezzo / (1 + (iva / 100));
+
+
+        }
+
+       
 
     }
 }
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace csharp_oop_shop
 {
-    internal class Program
+    public class Program
     {
+       
         static void Main(string[] args)
         {
-          
+            
+            Prodotto  mioProdotto = new Prodotto("portafoglio","Bel portafoglio", 9.99m, 22);
+
+            Console.WriteLine($"Il codice prodotto è: {mioProdotto.getCodice()}");
+            Console.WriteLine($"Il prezzo base  è: {mioProdotto.prezzo} ");
+            //Console.WriteLine($"Il prezzo senza iva  è: {mioProdotto.getPrezzoIva}");
+
         }
     }
 }
