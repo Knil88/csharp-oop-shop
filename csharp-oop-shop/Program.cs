1e63151c6423a7248c427cf5f5a1d16ea117a58b
Yari <narducciyarigmail.com>
2023-04-11 10:05:21 +0200

    cambio program

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -8,27 +8,27 @@
             Product product1 = new Product();
 
 
-            //Genera codice casuale da 8 cifre
+           
             Console.WriteLine($"Codice: {product1.GetCode()}");
 
-            //Print Price
+          
             Console.WriteLine("Set Price: ");
             product1.SetPrice(Convert.ToInt32(Console.ReadLine()));
             Console.WriteLine($"Prezzo: {product1.GetPrice()}$");
 
-            //Print Iva Price
+           
             Console.WriteLine($"Prezzo IVA: {product1.GetIvaPrice()}$");
 
 
-            //Set Name
+            
             Console.WriteLine("Set Name: ");
             product1.SetName(Console.ReadLine());
 
-            //product1.NameMoreCode(Console.ReadLine());
+           
             Console.WriteLine($"Nome: {product1.NameMoreCode(product1.nome, product1.GetCode())}");
 
 
-            //TEST
+           
             Console.WriteLine(product1.nome);
             Console.WriteLine(product1.prezzo);
 
